dccd1e1809b5b41aa1522072033e7df5aa16739e
hamzashamoun96 <hamza.shamoun96@gmail.com>
2020-12-23 16:34:52 +0200

    new

---
+++ detailsscript.js
@@ -0,0 +1,29 @@
+alert('Welocome To Sbay');
+alert('Buy More Than 10 And Get 50% SALE!!!');
+
+var numberofcars = prompt('How Many Cars Do You Want?');
+var Theprice = numberofcars * 199999;
+
+if (numberofcars < 1) {
+    alert('MAYBE NEXT TIME :(');
+}
+else if (numberofcars > 10) {
+    Theprice = Theprice / 2
+    alert('Total Price is ' + Theprice + '$');
+    confirm("Are You Sure of Buying This?");
+    alert('THANK YOU FOR BUYING FROM US :)');
+}
+else {
+    alert('Total Price is ' + Theprice + '$');
+    confirm("Are You Sure of Buying This?");
+    alert('THANK YOU FOR BUYING FROM US :)');
+}
+var recommendations =prompt('Any Recommendations?');
+alert('Your Recommendation Is : ' +recommendations)
+console.log(recommendations);
+
+
+
+
+
+
