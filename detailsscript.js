5e6950c24606c191cd99d6626cbbbadbf99673c8
hamzashamoun96 <hamza.shamoun96@gmail.com>
2020-12-24 12:45:18 +0200

    adding document.write

--- detailsscript.js
+++ detailsscript.js
@@ -1,7 +1,10 @@
 alert('Welocome To Sbay');
 alert('Buy More Than 10 And Get 50% SALE!!!');
-
+var name = prompt('Tell Us Your Name ');
+document.write('Your Name Is: ' + name + ' ');
 var numberofcars = prompt('How Many Cars Do You Want?');
+document.write('Number Of Cars Is: ' + numberofcars);
+
 var Theprice = numberofcars * 199999;
 
 if (numberofcars < 1) {
@@ -12,13 +15,16 @@ else if (numberofcars > 10) {
     alert('Total Price is ' + Theprice + '$');
     confirm("Are You Sure of Buying This?");
     alert('THANK YOU FOR BUYING FROM US :)');
+    document.write('Toltal Price: ' + Theprice + '$');
+
 }
 else {
     alert('Total Price is ' + Theprice + '$');
     confirm("Are You Sure of Buying This?");
     alert('THANK YOU FOR BUYING FROM US :)');
+    document.write('Toltal Price: ' + Theprice + '$');
 }
-var recommendations =prompt('Any Recommendations?');
+var recommendations = prompt('Any Recommendations?');
 alert('Your Recommendation Will Be considered')
 console.log(recommendations);
 
