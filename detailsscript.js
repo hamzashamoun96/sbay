8c179eb883e23db122c0ad4da2878059474a00ac
hamzashamoun96 <hamza.shamoun96@gmail.com>
2020-12-24 15:43:15 +0200

    For an While

--- detailsscript.js
+++ detailsscript.js
@@ -1,9 +1,15 @@
 alert('Welocome To Sbay');
 alert('Buy More Than 10 And Get 50% SALE!!!');
 var name = prompt('Tell Us Your Name ');
-document.write('<p>'+ 'Your Name Is: '+ name+'</p>');
+document.write('<p>' + 'Your Name Is: ' + name + '</p>');
+
 var numberofcars = prompt('How Many Cars Do You Want?');
-document.write('<p>'+'Number Of Cars Is: ' + numberofcars+'</p>');
+while (numberofcars === '') {
+    numberofcars = prompt('How Many Cars Do You Want?');
+}
+
+
+document.write('<p>' + 'Number Of Cars Is: ' + numberofcars + '</p>');
 
 var Theprice = numberofcars * 199999;
 
@@ -15,14 +21,19 @@ else if (numberofcars > 10) {
     alert('Total Price is ' + Theprice + '$');
     confirm("Are You Sure of Buying This?");
     alert('THANK YOU FOR BUYING FROM US :)');
-    document.write('Toltal Price: ' + Theprice + '$'+'</p>');
+    document.write('Toltal Price: ' + Theprice + '$' + '</p>');
 
 }
 else {
     alert('Total Price is ' + Theprice + '$');
     confirm("Are You Sure of Buying This?");
     alert('THANK YOU FOR BUYING FROM US :)');
-    document.write('<p>'+'Toltal Price: ' + Theprice + '$'+'<p>');
+    document.write('<p>' + 'Toltal Price: ' + Theprice + '$' + '<p>');
+}
+for (var i = 0; i < numberofcars; i++) {
+    
+    document.write(i+1 +'<img src="https://evchargeplus.com/wp-content/uploads/2019/11/BMW-i8-Coupe0-800x600.png" alt="">');
+    
 }
 var recommendations = prompt('Any Recommendations?');
 alert('Your Recommendation Will Be considered')
