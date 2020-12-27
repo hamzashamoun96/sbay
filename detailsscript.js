9fa834c5da544938c5843ea4b8aef9f10e746ce3
hamzashamoun96 <hamza.shamoun96@gmail.com>
2020-12-28 00:16:30 +0200

    hay

--- detailsscript.js
+++ detailsscript.js
@@ -1,18 +1,36 @@
-alert('Welocome To Sbay');
-alert('Buy More Than 10 And Get 50% SALE!!!');
-var name = prompt('Tell Us Your Name ');
-document.write('<p>' + 'Your Name Is: ' + name + '</p>');
+welcome('Welocome To Sbay')
+welcome('Buy More Than 10 And Get 50% SALE!!!')
 
-var numberofcars = prompt('How Many Cars Do You Want?');
-while (numberofcars === '') {
-    numberofcars = prompt('How Many Cars Do You Want?');
+var username = data('Tell Us Your Name ');
+var numberofcars = data('How Many Cars Do You Want?')
+
+username = important('Tell Us Your Name ', username);
+numberofcars = important('How Many Cars Do You Want?', numberofcars);
+
+
+document.write('<p>' + 'Your Name Is: ' + username + '</p>');
+document.write('<p>' + 'Number Of Cars Is: ' + numberofcars + '</p>');
+
+
+function welcome(message) {
+    alert(message)
+}
+
+function data(message) {
+    return prompt(message);
+}
+
+function important(message, write) {
+
+
+    while (write === '' || write === null) {
+        write = prompt(message);
+    }
+    return write;
 }
 
 
-document.write('<p>' + 'Number Of Cars Is: ' + numberofcars + '</p>');
-
 var Theprice = numberofcars * 199999;
-
 if (numberofcars < 1) {
     alert('MAYBE NEXT TIME :(');
 }
@@ -30,10 +48,11 @@ else {
     alert('THANK YOU FOR BUYING FROM US :)');
     document.write('<p>' + 'Toltal Price: ' + Theprice + '$' + '<p>');
 }
-for (var i = 0; i < numberofcars; i++) {
-    document.write('<p>' + (i + 1) + '</p>');
-    document.write('<img src="https://evchargeplus.com/wp-content/uploads/2019/11/BMW-i8-Coupe0-800x600.png" alt="">');
 
+
+for (var i = 1; i <= numberofcars; i++) {
+    document.write('<p>' + i + '</p>');
+    document.write('<img src="https://evchargeplus.com/wp-content/uploads/2019/11/BMW-i8-Coupe0-800x600.png" alt="">');
 }
 var recommendations = prompt('Any Recommendations?');
 alert('Your Recommendation Will Be considered')
